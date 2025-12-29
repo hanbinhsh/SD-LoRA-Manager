702b9bea2dffc9ebf75261922aa2633a4b188585
IceRinne <1024622242@qq.com>
2025-12-29 13:08:48 +0800

    默认启动时显示黑暗模式

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QStyleHints>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    a.styleHints()->setColorScheme(Qt::ColorScheme::Dark);
     a.setWindowIcon(QIcon("://logo.ico"));
     MainWindow w;
     w.show();
